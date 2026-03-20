5f4c6a738fb9a970cb490f7edc10a72695a0d37a
PhantomStudioX <expo-builder@As-MacBook.local>
2026-03-20 11:30:07 -0500

    Added PWA support

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 registerRootComponent(App);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
